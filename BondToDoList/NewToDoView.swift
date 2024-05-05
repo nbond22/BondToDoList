a31c6f52329cbc4a898f381ff3e2f9d7662f9b6c
Nicole Bond <nicbond22@gmail.com>
2024-05-04 21:13:24 -0400

    Interation 0 complete!

---
+++ BondToDoList/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  BondToDoList
+//
+//  Created by Scholar on 5/4/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
