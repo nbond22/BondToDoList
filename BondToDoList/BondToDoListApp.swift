1919d81b3706a40d932988df26cadb93ab09aa7b
Nicole Bond <nicbond22@gmail.com>
2024-06-26 09:52:00 -0400

    Changed "To Do List" to "To-Do List"

--- BondToDoList/BondToDoListApp.swift
+++ BondToDoList/BondToDoListApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BondToDoListApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: toDoItem.self)
         }
     }
 }
--- BondToDoList/NewToDoView.swift
+++ BondToDoList/NewToDoView.swift
@@ -6,13 +6,61 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    
+    @Binding var showNewTask: Bool
+    @Bindable var toDoItem: toDoItem
+    @Environment(\.modelContext) var modelContext
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Text("What do you need to do?")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.purple).multilineTextAlignment(.center)
+            
+            TextField("Enter the task description here...", text: $toDoItem.title, axis: .vertical)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(20)
+            
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is it important?")
+                    .fontWeight(.semibold)
+                    .italic()
+                    .foregroundColor(.red)
+            
+            }
+            
+            Button {
+                addToDo()
+                self.showNewTask = false
+            } label: {
+                Text("Save")
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.green)
+            }
+        }
+        .padding(20)
     }
+
+    func addToDo () {
+        let toDo = BondToDoList.toDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
+    }
+    
 }
 
+
 #Preview {
-    NewToDoView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: toDoItem.self, configurations: config)
+
+    let toDo = toDoItem(title: "", isImportant: false)
+    return NewToDoView(showNewTask: .constant(true), toDoItem: toDo)
+        .modelContainer(container)
 }
